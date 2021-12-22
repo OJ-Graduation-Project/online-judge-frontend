fd66ac2de378393b9b76ac1cda82cf32755ac972
VerginiaEhab-incorta <verginia.ehab@incorta.com>
2021-12-22 14:28:59 +0200

    updated table

--- src/pages/home/components/table.tsx
+++ src/pages/home/components/table.tsx
@@ -1,355 +1,104 @@
 import * as React from "react";
-import { alpha } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
-import TableSortLabel from "@mui/material/TableSortLabel";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
-import { visuallyHidden } from "@mui/utils";
-
-interface Data {
-  difficulty: string;
-  status: string;
-  name: string;
-}
-
-function createData(name: string, difficulty: string, status: string): Data {
-  return {
-    name,
-    difficulty,
-    status,
-  };
-}
+const columns: GridColDef[] = [
+  { field: "id", headerName: "Title", width: 800 },
+  { field: "status", headerName: "Status", width: 300 },
+  { field: "difficulty", headerName: "Difficulty", width: 300 },
+];
 
 const rows = [
-  createData("9. Palindrome Number", "Solved", "Easy"),
-  createData("119. Pascal's Triangle II", "Attempted", "Easy"),
-  createData("121. Best Time to Buy and Sell Stock", "Todo", "Easy"),
-  createData("1752. Check if Array Is Sorted and Rotated", "Solved", "Easy"),
-  createData("1736. Latest Time by Replacing Hidden Digits", "Todo", "Easy"),
-  createData("7. Reverse Integer", "Solved", "Medium"),
-  createData("19. Remove Nth Node From End of List", "Solved", "Medium"),
-  createData("50. Pow(x, n)", "Solved", "Medium"),
-  createData("61. Rotate List", "Todo", "Medium"),
-  createData("63. Unique Paths II", "Todo", "Medium"),
-  createData("23. Merge k Sorted Lists", "Solved", "Hard"),
-  createData("32. Longest Valid Parentheses", "Todo", "Hard"),
-  createData("41. First Missing Positive", "Todo", "Hard"),
-  createData("68. Text Justification", "Todo", "Hard"),
-  createData("84. Largest Rectangle in Histogram", "Todo", "Hard"),
-];
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-type Order = "asc" | "desc";
-
-function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key
-): (
-  a: { [key in Key]: number | string },
-  b: { [key in Key]: number | string }
-) => number {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
-function stableSort<T>(
-  array: readonly T[],
-  comparator: (a: T, b: T) => number
-) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-interface HeadCell {
-  disablePadding: boolean;
-  id: keyof Data;
-  label: string;
-  numeric: boolean;
-}
-
-const headCells: readonly HeadCell[] = [
   {
-    id: "name",
-    numeric: false,
-    disablePadding: true,
-    label: "Problem id.Title",
+    id: "84. Largest Rectangle in Histogram",
+    difficulty: "Hard",
+    status: "Todo",
   },
   {
-    id: "difficulty",
-    numeric: true,
-    disablePadding: true,
-    label: "Status",
+    id: "68. Text Justification",
+    difficulty: "Hard",
+    status: "Todo",
   },
   {
-    id: "status",
-    numeric: true,
-    disablePadding: false,
-    label: "Difficulty",
+    id: "41. First Missing Positive",
+    difficulty: "Hard",
+    status: "Todo",
+  },
+  {
+    id: "32. Longest Valid Parentheses",
+    difficulty: "Hard",
+    status: "Todo",
+  },
+  {
+    id: "23. Merge k Sorted Lists",
+    difficulty: "Hard",
+    status: "Solved",
+  },
+  {
+    id: "63. Unique Paths II",
+    difficulty: "Medium",
+    status: "Todo",
+  },
+  {
+    id: "61. Rotate List",
+    difficulty: "Medium",
+    status: "Todo",
+  },
+  {
+    id: "50. Pow(x, n)",
+    difficulty: "Medium",
+    status: "Solved",
+  },
+  {
+    id: "50. Pow(x, n)",
+    difficulty: "Medium",
+    status: "Solved",
+  },
+  {
+    id: "19. Remove Nth Node From End of List",
+    difficulty: "Medium",
+    status: "Solved",
+  },
+  {
+    id: "7. Reverse Integer",
+    difficulty: "Medium",
+    status: "Solved",
+  },
+  {
+    id: "1736. Latest Time by Replacing Hidden Digits",
+    difficulty: "Easy",
+    status: "Todo",
+  },
+  {
+    id: "1752. Check if Array Is Sorted and Rotated",
+    difficulty: "Easy",
+    status: "Todo",
+  },
+  {
+    id: "121. Best Time to Buy and Sell Stock",
+    difficulty: "Easy",
+    status: "Todo",
+  },
+  {
+    id: "119. Pascal's Triangle II",
+    difficulty: "Easy",
+    status: "Todo",
+  },
+  {
+    id: "9. Palindrome Number",
+    difficulty: "Easy",
+    status: "Todo",
   },
 ];
 
-interface EnhancedTableProps {
-  numSelected: number;
-  onRequestSort: (
-    event: React.MouseEvent<unknown>,
-    property: keyof Data
-  ) => void;
-  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  order: Order;
-  orderBy: string;
-  rowCount: number;
-}
-
-function EnhancedTableHead(props: EnhancedTableProps) {
-  const {
-    order,
-    orderBy,
-
-    onRequestSort,
-  } = props;
-  const createSortHandler =
-    (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
-      onRequestSort(event, property);
-    };
-
+export default function DataTable() {
   return (
-    <TableHead>
-      <TableRow>
-        <TableCell padding="checkbox"></TableCell>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? "right" : "left"}
-            padding={headCell.disablePadding ? "none" : "normal"}
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : "asc"}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === "desc" ? "sorted descending" : "sorted ascending"}
-                </Box>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-
-interface EnhancedTableToolbarProps {
-  numSelected: number;
-}
-
-const EnhancedTableToolbar = (props: EnhancedTableToolbarProps) => {
-  const { numSelected } = props;
-
-  return (
-    <Toolbar
-      sx={{
-        pl: { sm: 2 },
-        pr: { xs: 1, sm: 1 },
-        ...(numSelected > 0 && {
-          bgcolor: (theme) =>
-            alpha(
-              theme.palette.primary.main,
-              theme.palette.action.activatedOpacity
-            ),
-        }),
-      }}
-    >
-      {numSelected > 0 ? (
-        <Typography
-          sx={{ flex: "1 1 100%" }}
-          color="inherit"
-          variant="subtitle1"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography
-          sx={{ flex: "1 1 100%" }}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          Problems
-        </Typography>
-      )}
-    </Toolbar>
-  );
-};
-
-export default function EnhancedTable() {
-  const [order, setOrder] = React.useState<Order>("asc");
-  const [orderBy, setOrderBy] = React.useState<keyof Data>("difficulty");
-  const [selected, setSelected] = React.useState<readonly string[]>([]);
-  const [page, setPage] = React.useState(0);
-  const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const handleRequestSort = (
-    event: React.MouseEvent<unknown>,
-    property: keyof Data
-  ) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.name);
-      setSelected(newSelecteds);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected: readonly string[] = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
-    }
-
-    setSelected(newSelected);
-  };
-
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  const isSelected = (name: string) => selected.indexOf(name) !== -1;
-
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-
-  return (
-    <Box sx={{ width: "100%" }}>
-      <Paper sx={{ width: "100%", mb: 2 }}>
-        <EnhancedTableToolbar numSelected={selected.length} />
-        <TableContainer>
-          <Table
-            sx={{ minWidth: 750 }}
-            aria-labelledby="tableTitle"
-            size={dense ? "small" : "medium"}
-          >
-            <EnhancedTableHead
-              numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
-              onRequestSort={handleRequestSort}
-              rowCount={rows.length}
-            />
-            <TableBody>
-              {/* if you don't need to support IE11, you can replace the `stableSort` call with:
-              rows.slice().sort(getComparator(order, orderBy)) */}
-              {stableSort(rows, getComparator(order, orderBy))
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => {
-                  const isItemSelected = isSelected(row.name);
-                  const labelId = `enhanced-table-checkbox-${index}`;
-
-                  return (
-                    <TableRow
-                      hover
-                      onClick={(event) => handleClick(event, row.name)}
-                      role="checkbox"
-                      aria-checked={isItemSelected}
-                      tabIndex={-1}
-                      key={row.name}
-                      selected={isItemSelected}
-                    >
-                      <TableCell padding="checkbox"></TableCell>
-                      <TableCell
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        padding="none"
-                      >
-                        {row.name}
-                      </TableCell>
-                      <TableCell align="right">{row.difficulty}</TableCell>
-                      <TableCell align="right">{row.status}</TableCell>
-                    </TableRow>
-                  );
-                })}
-              {emptyRows > 0 && (
-                <TableRow
-                  style={{
-                    height: (dense ? 33 : 53) * emptyRows,
-                  }}
-                >
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
-    </Box>
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+      />
+    </div>
   );
 }
