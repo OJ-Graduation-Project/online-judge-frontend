dad9e7a9ab1b028f1bf384bb0840e0bdf174fdb2
mohamed99ayman <mohamed99ayman250@gmail.com>
2022-01-02 19:43:15 +0200

    add fetch and remove axiom

--- src/pages/sign-up/components/signupForm.tsx
+++ src/pages/sign-up/components/signupForm.tsx
@@ -3,7 +3,25 @@ import styles from "./styles.module.css";
 import { Button, TextField, Divider } from '@material-ui/core';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 const SignUpForm=()=>{
+ 
+    const [firstname, setfirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+ 
+    const handleSubmit=()=>{
+        const user ={firstname,lastname,email,password};
+        fetch('http://localhost:8000/sign-up',{
+            method : 'POST',
+            headers:{'content-type':'application/json'},
+            body:JSON.stringify(user)
+        }).then(()=>{
+            console.log("done");
+        })
+    }
+ 
     return(
         <div>
             <div className={styles["icon"]}>
@@ -14,16 +32,16 @@ const SignUpForm=()=>{
             </div>
             <div className={styles["icon"]}>
                 <div className='col-6 p-2'>
-                    <TextField id="firstname" style={{margin:5}} type="text" variant="outlined" label="Enter First Name"/>
+                    <TextField id="firstname" style={{margin:5}} type="text" value={firstname} onChange={(e)=>setfirstname(e.target.value)} variant="outlined" label="Enter First Name"/>
                 </div>
                 <div className='col-6 p-2'>
-                    <TextField id="lastname" style={{margin:5}} type="text" variant="outlined" label="Enter Last Name"/>
+                    <TextField id="lastname" style={{margin:5}} type="text" value={lastname} onChange={(e)=>setLastname(e.target.value)} variant="outlined" label="Enter Last Name"/>
                 </div>
             </div>
             <div className={styles["icon"]}>
-                <TextField id="email" className="p-2" style={{margin:5}} type="text" variant="outlined" label="Enter Email"/>
-                <TextField id="password" className="p-2" style={{margin:5}} type="password" variant="outlined" label="Enter Password"/>
-                <Button variant='contained' color="primary" onClick={(event) => (window.location.href = "/login")}>Create Account</Button>
+                <TextField id="email" className="p-2" style={{margin:5}} type="text" value={email} onChange={(e)=>setEmail(e.target.value)} variant="outlined" label="Enter Email"/>
+                <TextField id="password" className="p-2" style={{margin:5}} type="password" value={password} onChange={(e)=>setPassword(e.target.value)} variant="outlined" label="Enter Password"/>
+                <Button variant='contained' color="primary" onClick={handleSubmit}>Create Account</Button>
             
                 <Divider variant="middle"/>
                 <p className='text-center'>
@@ -32,7 +50,7 @@ const SignUpForm=()=>{
                     </Link>
                 </p>
             </div>
-
+ 
         </div>
     )
 }
