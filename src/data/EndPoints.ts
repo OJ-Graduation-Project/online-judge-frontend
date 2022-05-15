e630c51e55de8fbcb12e6cebbfb572ab55c2547c
Omar Radwan <omarradwanx@gmail.com>
2022-05-15 03:41:19 +0200

    Modify Frontend and Backend to Run on Same Host in Kubernetes Setup
    Close OJ-109
    
    * Add subPath, connectionType to configuration
    * Fix topic url

--- src/data/EndPoints.ts
+++ src/data/EndPoints.ts
@@ -3,29 +3,32 @@ import KubernetesConfig from "../config/kubernetes_config.json"
 
 
 let backend_url: string
+let connection_type: string
 switch (process.env.REACT_APP_CONFIG) {
     case 'kubernetes': {
-        backend_url = `${KubernetesConfig.backend.host}:${KubernetesConfig.backend.port}`
+        backend_url = `${KubernetesConfig.backend.host}:${KubernetesConfig.backend.port}${KubernetesConfig.backend.subPath}`
+        connection_type = KubernetesConfig.backend.connectionType
         break;
     }
     default: {
-        backend_url = `${LocalHostConfig.backend.host}:${LocalHostConfig.backend.port}`
+        backend_url = `${LocalHostConfig.backend.host}:${LocalHostConfig.backend.port}${LocalHostConfig.backend.subPath}`
+        connection_type = LocalHostConfig.backend.connectionType
         break;
     }
 }
-export const ALL_CONTESTS_URL = `http://${backend_url}/all-contests`;
-export const CONTEST_URL = `http://${backend_url}/all-contests/contest/`;
-export const CREATE_CONTEST_URL = `http://${backend_url}/create-contest`;
-export const CREATE_PROBLEM_URL = `http://${backend_url}/create-problem`;
-export const HOME_URL = `http://${backend_url}/home`;
-export const LOGIN_URL = `http://${backend_url}/login`;
-export const LOGOUT_URL = `http://${backend_url}/logout`;
-export const PROBLEM_URL = `http://${backend_url}/problem`;
-export const PROFILE_URL = `http://${backend_url}/profile`;
-export const REGISTRATION_URL = `http://${backend_url}/all-contests/Registration/contest-name`;
-export const SIGNUP_URL = `http://${backend_url}/sign-up`;
-export const SUBMIT_URL = `http://${backend_url}/submit`;
-export const TOPICS_URL = "http://${AppConfig.backend.host}:${AppConfig.backend.port}/topic";
-export const USER_PROBLEMS_URL = `http://${backend_url}/user-problems/`;
-export const USER_SUBMISSIONS_URL = `http://${backend_url}/user-submissions/`;
+export const ALL_CONTESTS_URL = `${connection_type}://${backend_url}/all-contests`;
+export const CONTEST_URL = `${connection_type}://${backend_url}/all-contests/contest/`;
+export const CREATE_CONTEST_URL = `${connection_type}://${backend_url}/create-contest`;
+export const CREATE_PROBLEM_URL = `${connection_type}://${backend_url}/create-problem`;
+export const HOME_URL = `${connection_type}://${backend_url}/home`;
+export const LOGIN_URL = `${connection_type}://${backend_url}/login`;
+export const LOGOUT_URL = `${connection_type}://${backend_url}/logout`;
+export const PROBLEM_URL = `${connection_type}://${backend_url}/problem`;
+export const PROFILE_URL = `${connection_type}://${backend_url}/profile`;
+export const REGISTRATION_URL = `${connection_type}://${backend_url}/all-contests/Registration/contest-name`;
+export const SIGNUP_URL = `${connection_type}://${backend_url}/sign-up`;
+export const SUBMIT_URL = `${connection_type}://${backend_url}/submit`;
+export const TOPICS_URL = `${connection_type}://${backend_url}/topic`;
+export const USER_PROBLEMS_URL = `${connection_type}://${backend_url}/user-problems/`;
+export const USER_SUBMISSIONS_URL = `${connection_type}://${backend_url}/user-submissions/`;
 
