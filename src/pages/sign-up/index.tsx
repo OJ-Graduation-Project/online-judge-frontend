618f9d03d1360b8a104f23bbd29638bd63a86842
youssefhusseinn <youssef.hussein42@gmail.com>
2022-05-17 18:42:32 +0200

    Refactored SignUp frontend

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,13 +1,188 @@
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import React from "react";
-import styles from "./styles.module.css";
-import SignUpForm from "./components/signupForm";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { SIGNUP_URL } from "../../data/EndPoints";
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+const theme = createTheme();
 
 const SignUp: React.FC = () => {
+  const validateEmail = (email: string) => {
+    let regex = new RegExp(
+      "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+    );
+    return regex.test(email);
+  };
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [country, setCountry] = useState("");
+  const [organization, setOrganization] = useState("");
+  const [acceptedCount, setAcceptedCount] = useState(0);
+  const [wrongCount, setWrongCount] = useState(0);
+  const [timeLimitExceededCount, setTimeLimitExceededCount] = useState(0);
+  const [runtimeCount, setRuntimeCount] = useState(0);
+
+  const [cookie, setCookie] = useState(cookies.get("cookie"))
+  const navigate = useNavigate();
+  useEffect(() => {
+      if(cookie != null){
+          navigate("/home")
+      }
+  });
+  const handleClick = () => {
+    const user = {
+      firstName,
+      lastName,
+      country,
+      organization,
+      acceptedCount,
+      wrongCount,
+      timeLimitExceededCount,
+      runtimeCount,
+      email,
+      password,
+    };
+    fetch(SIGNUP_URL, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        if(json.message) {
+          alert(json.message)
+        }
+        else{
+          navigate("/login")
+        }
+      });
+  };
+
   return (
-    <div className={styles["SignUp"]}>
-      <SignUpForm/>
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <Box sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  name="firstName"
+                  autoComplete="firstName"
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lastName"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="country"
+                  label="Country"
+                  name="country"
+                  autoComplete="country"
+                  onChange={(e) => setCountry(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="organization"
+                  label="Organization"
+                  name="organization"
+                  autoComplete="organization"
+                  onChange={(e) => setOrganization(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}></Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleClick}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link to="/login" className="text-black-50">
+                  <h5>Already have an account?</h5>
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default SignUp;
