77e22e8a1ddaf977c2fc9d856e04e96750cef9b4
youssefhusseinn <youssef.hussein42@gmail.com>
2022-05-17 16:01:55 +0200

    - Changed UI. - Refactored.

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
-import styles from "./styles.module.css";
-import { Button, TextField, Divider } from '@material-ui/core';
-import { Link, useNavigate } from 'react-router-dom';
-import PersonIcon from '@material-ui/icons/Person';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React from 'react';
+import { useState } from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import { LOGIN_URL } from "../../data/EndPoints";
+
 import Cookies from 'universal-cookie';
-import {LOGIN_URL} from "../../data/EndPoints"
 const cookies = new Cookies();
+const theme = createTheme();
 
 const Login: React.FC = () =>{
     const [email, setEmail] = useState('');
@@ -21,39 +31,83 @@ const Login: React.FC = () =>{
         }).then((res) => res.json())
         .then((json) => {
             console.log(json)
-            // navigate("/login");
-            setCookie(cookies.get("cookie"))
-            console.log(cookie)
-            navigate("/home")
+            if(json.message) {
+                alert(json.message)
+            }
+            else{
+                setCookie(cookies.get("cookie"))
+                navigate("/home")
+            }
         })
     }
 
-    return(
-        <div>
-            
-            <div className={styles["Login"]}>
-                <div className={styles["icon"]}>
-                    <div className={styles["icon_class"]}>
-                        <PersonIcon fontSize="large"/>
-                    </div>
-                    <div className={styles["text"]}>Sign In</div>
-                </div>
-                <div className={styles["icon"]}>
-                    <TextField id="email" style={{margin:5}} type="text"  value={email} onChange={(e)=>setEmail(e.target.value)} variant="outlined" label="Enter Email"/>
-                    <TextField id="password" style={{margin:5}} type="password" value={password} onChange={(e)=>setPassword(e.target.value)} variant="outlined" label="Enter Password"/>
-                    <div>
-                    <Button variant='contained' color="primary" onClick={handleClick}>Login</Button>    
-                    <Divider variant="middle"/>
-                    <p className='text-center'>
-                        <Link to="/sign-up" className="text-black-50">
-                            <h5>Register?</h5>
-                        </Link>
-                    </p>
-                    </div>
-                </div> 
-            </div>
-        </div>
-    )
+    return (
+        <ThemeProvider theme={theme}>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Login
+              </Typography>
+              <Box sx={{ mt: 3 }}>
+                <Grid container spacing={2}>
+                <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  onChange={(e)=>setEmail(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={(e)=>setPassword(e.target.value)}
+                />
+                  </Grid>
+                  <Grid item xs={12}>
+                  </Grid>
+                </Grid>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                  onClick={handleClick}
+                >
+                    Login
+                </Button>
+                <Grid container justifyContent="flex-end">
+                  <Grid item>
+                  <Link to="/sign-up" className="text-black-50">
+                    <h5>First time? Sign up</h5>
+                </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+          </Container>
+        </ThemeProvider>
+      );
 }
 
 export default Login;
